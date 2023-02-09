3f518f6d03ac420441cd0149f2d1723a361775a7
jsarabia <72255876+javiburn@users.noreply.github.com>
2023-02-09 16:43:15 +0100

    WIT

--- get_next_line.c
+++ get_next_line.c
@@ -5,51 +5,73 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-
-/*
-char *temp(int fd)
+static int	ft_checkpos(char *str)
 {
-	char *temp;
-	char	*new;
+	int	n;
 
-	temp = r(fd);
-	while (
+	n = 0;
+	while (str[n] != '\0' && str[n] != '\n')
+		n++;
+	if (str[n] == '\n')
+		return (n);
+	return (-1);
 }
-*/
-char *r(int fd)
+
+static char	*ft_reading(int fd)
 {
-	static char	*ptr;
-	char		*temp;
+	static char	*buf;
+	char		*ret;
+	char		*end;
 	ssize_t		hola;
 	int			i;
 
-	i = 0;
 	if (fd == -1)
 		return (NULL);
 	else
 	{
-		ptr = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
-		hola = read(fd, ptr, BUFFER_SIZE);
+		buf = (char *)malloc((BUFFER_SIZE) * sizeof(char) + 1);
+		hola = read(fd, buf, BUFFER_SIZE);
+		if (ft_checkpos(buf))
+		{
+			ret = ft_substr(buf, 0, ft_checkpos(buf));
+			*buf = *(buf) - ft_checkpos(buf);
+			return (ret);
+		}
+		else if (ft_checkpos(buf) == -1)
+			ret = ft_substr(buf, 0, BUFFER_SIZE);
+		while (ft_checkpos(buf) == -1)
+		{
+			if (i == 1)
+				i = 2;
+			hola = read(fd, buf, BUFFER_SIZE);
+			i++;
+		}
+		ret = ft_strjoin(ret, buf);
+		//strjoin
 	}
-	while (ptr[i] != '\n' && i < BUFFER_SIZE)
-		i++;
-	if (ptr[i] == '\n')
-	{
-		temp = ft_substr(ptr, 0, i);
-		return (temp);
-	}
-	else
-		r(fd);
-	return (NULL);
+	return (ret);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*str;
+
+	str = ft_reading(fd);
+	return (str);
 }
 
 int main()
 {
 	char	*ptr;
 	int fd;
+	int n = 0;
 
 	fd = open("/Users/jsarabia/C_Cursus/get_next_line/hola.txt", O_RDONLY);
-	ptr = r(fd);
-	printf("%s", ptr);
+	while (n < 16)
+	{
+		ptr = get_next_line(fd);
+		printf("%s\n", ptr);
+		n++;
+	}
 	return (0);
 }
