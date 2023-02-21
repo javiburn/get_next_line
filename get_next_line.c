3641e9c73d063e852544dbc07bada042d170dcc2
jsarabia <72255876+javiburn@users.noreply.github.com>
2023-02-21 15:12:48 +0100

    Perfecto a falta de normi

--- get_next_line.c
+++ get_next_line.c
@@ -3,125 +3,156 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsarabia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jsarabia <jsarabia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 16:03:00 by jsarabia          #+#    #+#             */
-/*   Updated: 2023/02/09 17:04:42 by jsarabia         ###   ########.fr       */
+/*   Updated: 2023/02/21 15:07:29 by jsarabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "get_next_line.h"
 
-
-static int	ft_checkpos(char *str)
+static char	*ft_store(char *r)
 {
-	int 		n;
+	char	*store;
+	char	*temp;
+
+	if (ft_strchr(r, '\n') == NULL && r)
+	{
+		free(r);
+		return (NULL);
+	}
+	temp = ft_strchr(r, '\n');
+	if (!temp)
+	{
+		free(r);
+		return (NULL);
+	}
+	store = ft_calloc(ft_strlen(temp), sizeof(char));
+	ft_strlcpy(store, temp + 1, ft_strlen(temp));
+	if (!store)
+	{
+		free(r);
+		free (temp);
+		return (NULL);
+	}
+	free(r);
+	return (store);
+}
+
+static char	*ft_returning(char *r)
+{
+	int		n;
+	char	*str;
 
 	n = 0;
-	while (str[n] != '\0' && str[n] != '\n')
-		n++;
-	if (str[n] == '\n')
-		return (n);
-	return (-1);
-}
-
-static int	ft_lenspace(char *str)
-{
-	int	lenspace;
-	int	nextspace;
-
-	lenspace = 1;
-	nextspace = ft_checkpos(str) + 1;
-	while (str[nextspace] != '\n' && nextspace <= BUFFER_SIZE)
+	if (!r)
 	{
-		nextspace++;
-		lenspace++;
-	}
-	return (lenspace);
-}
-
-static char	*ft_create(char *str)
-{
-	static char	start[BUFFER_SIZE];
-	char		*ret;
-
-	ret = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
-	ret = ft_strjoin(start, (char const *)str);
-	if (!ret)
+		free(r);
 		return (NULL);
-	ft_strlcpy(start, str + ft_checkpos(str), BUFFER_SIZE - ft_checkpos(str) + 1);
-	return (ret);
+	}
+	while (r[n] != '\0' && r[n] != '\n')
+		n++;
+	if (r[n] != '\0')
+		n = n + 1;
+	str = ft_calloc(n + 1, sizeof(char));
+	ft_strlcpy(str, r, n + 1);
+	if (!str || str[0] == '\0')
+	{
+		free(str);
+		return (NULL);
+	}
+	return (str);
 }
 
-static char	*ft_reading(int fd)
+static char	*ft_bufman(char *temp, char *r)
 {
-	static char	*buf;
-	char		*toret;
-	ssize_t		hello;
+	char	*newbuf;
 
-	if (fd == -1)
+	if (temp[0] == '\0')
 		return (NULL);
 	else
+		newbuf = ft_strjoin(r, temp);
+	free(r);
+	return (newbuf);
+}
+
+static char	*ft_reading(int fd, char *r)
+{
+	char		*buf;
+	char		*temp;
+	ssize_t		hola;
+
+	if (r == NULL)
+		r = (char *)ft_calloc(1, 1);
+	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	hola = read(fd, buf, BUFFER_SIZE);
+	if (hola < 0 || ((!buf || buf[0] == '\0' || hola == 0) && !r))
 	{
-		buf = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
-		hello = read(fd, buf, BUFFER_SIZE);
-		if (hello == 0)
-			return (NULL);
+		free(r);
+		free(buf);
+		return (NULL);
 	}
-	toret = ft_strdup((const char *)(buf));
-	return (toret);
+	temp = ft_calloc((int)hola + 1, sizeof(char));
+	ft_strlcpy(temp, buf, (int)hola + 1);
+	while (hola > 0)
+	{
+		r = ft_bufman(temp, r);
+		if (ft_strchr(buf, '\n') != NULL)
+			break ;
+		hola = read(fd, buf, BUFFER_SIZE);
+		if (hola == 0)
+			break ;
+		free(temp);
+		temp = ft_calloc((int)hola + 1, sizeof(char));
+		ft_strlcpy(temp, buf, (int)hola + 1);
+	}
+	free(buf);
+	free(temp);
+	return (r);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*temp;
-	char		*temp2;
+	static char	*r;
 	char		*str;
-	static int	check = 0;
-	static int	antlen = 0;
 
-	temp = ft_strdup(ft_reading(fd));
-	if (!temp)
+	if (fd < 0 || BUFFER_SIZE <= 0 || 0 > read(fd, 0, 0))
+	{
+		if (r)
+		{
+			free(r);
+			r = NULL;
+		}
 		return (NULL);
-	if (ft_checkpos(temp) != -1)
-	{
-		str = malloc((antlen + ft_checkpos(temp)) * sizeof(char) + 1);
-		ft_strlcpy(str, ft_create(temp), antlen + ft_checkpos(temp) + 1);
-		antlen = ft_lenspace(temp);
-		free(temp);
-		return (str);
 	}
-	ft_strlcpy(str, temp, BUFFER_SIZE + 1);
-	while (ft_checkpos(temp) == -1)
+	r = ft_reading(fd, r);
+	if (r == NULL)
 	{
-		free(temp2);
-		temp2 = malloc(BUFFER_SIZE * sizeof(char) + 1);
-		temp2 = ft_strdup(ft_reading(fd));
-		ft_strlcpy(str, temp2, BUFFER_SIZE);
+		free(r);
+		return (NULL);
 	}
-	//WIT: -queda hacer que se almacene la longitud del string temp
-	//     -queda por poner lo que pasa si no hay \n (que vuelva al bucle)
+	str = ft_returning(r);
+	r = ft_store(r);
 	return (str);
 }
-
+/*
 int main()
 {
 	char	*ptr;
 	int fd;
 	int n = 0;
-	system("leaks -q a.out");
-	fd = open("/Users/jsarabia/C_Cursus/get_next_line/hola.txt", O_RDONLY);
-	while (n < 7)
+	//system("leaks -q a.out");
+	fd = open("hola.txt", O_RDONLY);
+	while (n < 700)
 	{
 		ptr = get_next_line(fd);
-		printf("%s\n", ptr);
+		if (ptr == NULL)
+			break ;
+		printf("%s", ptr);
 		n++;
 	}
-	system("leaks -q a.out");
+	//system("leaks -q a.out");
 	return (0);
 }
+*/
